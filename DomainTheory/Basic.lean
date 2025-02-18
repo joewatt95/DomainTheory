223a0049a4ed817142ac0efff2860db383a2db00
Joe Watt <joewatt95@u.nus.edu>
2025-02-18 22:42:24 +0800

    tidy

--- DomainTheory/Basic.lean
+++ DomainTheory/Basic.lean
@@ -50,20 +50,20 @@ lemma lfp_eq_lfpApprox_ord_of_fixed_point
       lfp f
   _ = lfpApprox f ⊥ ord' := Eq.symm ‹_›
   _ = lfpApprox f ⊥ ord :=
-      have :
-        (ord' ≤ ord → lfpApprox f ⊥ ord = lfpApprox f ⊥ ord') ∧
-        (ord' ≥ ord → lfpApprox f ⊥ ord' = lfpApprox f ⊥ ord) := by
-        refine ⟨?_, ?_⟩
-        repeat
-          intro
-          apply lfpApprox_eq_of_mem_fixedPoints
-          . exact OrderBot.bot_le _
-          . assumption
-          . simp_all only [Function.mem_fixedPoints, Function.IsFixedPt, map_lfp]
+    have :
+      (ord' ≤ ord → lfpApprox f ⊥ ord = lfpApprox f ⊥ ord') ∧
+      (ord' ≥ ord → lfpApprox f ⊥ ord' = lfpApprox f ⊥ ord) := by
+      refine ⟨?_, ?_⟩
+      repeat
+        intro
+        apply lfpApprox_eq_of_mem_fixedPoints
+        . exact OrderBot.bot_le _
+        . assumption
+        . simp_all only [Function.mem_fixedPoints, Function.IsFixedPt, map_lfp]
 
-      match le_total _ _ with
-      | .inl (_ : ord' ≤ ord) | .inr (_ : ord' ≥ ord) =>
-        by simp_all only [map_lfp, forall_const, ge_iff_le]
+    match le_total _ _ with
+    | .inl (_ : ord' ≤ ord) | .inr (_ : ord' ≥ ord) =>
+      by simp_all only [map_lfp, forall_const, ge_iff_le]
 
 -- This proof is annoying as we need to juggle casting between `ℕ` and `Ordinal`
 lemma lfpApprox_omega0_eq_sSup_lfpApprox_Nat :
@@ -72,30 +72,30 @@ lemma lfpApprox_omega0_eq_sSup_lfpApprox_Nat :
       sSup {lfpApprox f ⊥ n | n : ℕ}
 
   _ = sSup {a | a = lfpApprox f ⊥ 0 ∨ ∃ n : ℕ, a = lfpApprox f ⊥ (n + 1)} :=
-      congr_arg _ <| Set.ext λ _ ↦ by
-        simp only [Set.mem_setOf_eq, Nat.cast_add, Nat.cast_one, add_one_eq_succ]
-        rw [←Nat.or_exists_add_one]
-        aesop
+    congr_arg _ <| Set.ext λ _ ↦ by
+      simp only [Set.mem_setOf_eq, Nat.cast_add, Nat.cast_one, add_one_eq_succ]
+      rw [←Nat.or_exists_add_one]
+      aesop
 
   _ = sSup ({lfpApprox f ⊥ 0} ∪ {f <| lfpApprox f ⊥ n | n : ℕ}) :=
-      congr_arg _ <|
-        have := lfpApprox_add_one_eq (f := f)
-        by aesop
+    congr_arg _ <|
+      have := lfpApprox_add_one_eq (f := f)
+      by aesop
 
   _ = sSup {f <| lfpApprox f ⊥ n | n : ℕ} :=
-      have := calc
-            lfpApprox f ⊥ 0
-          ≤ lfpApprox f ⊥ 1     := lfpApprox_monotone _ _ <| Ordinal.zero_le _
-        _ = f (lfpApprox f ⊥ 0) := by rw [←lfpApprox_add_one_eq, zero_add]
+    have := calc
+          lfpApprox f ⊥ 0
+        ≤ lfpApprox f ⊥ 1     := lfpApprox_monotone _ _ <| Ordinal.zero_le _
+      _ = f (lfpApprox f ⊥ 0) := by rw [←lfpApprox_add_one_eq, zero_add]
 
-      have : lfpApprox f ⊥ 0 ≤ sSup {f <| lfpApprox f ⊥ n | n : ℕ} :=
-        le_sSup_of_le ⟨0, by simp only [Nat.cast_zero]⟩ this
+    have : lfpApprox f ⊥ 0 ≤ sSup {f <| lfpApprox f ⊥ n | n : ℕ} :=
+      le_sSup_of_le ⟨0, by simp only [Nat.cast_zero]⟩ this
 
-      by simp_all only [add_one_eq_succ, Set.singleton_union, sSup_insert, sup_of_le_right]
+    by simp_all only [add_one_eq_succ, Set.singleton_union, sSup_insert, sup_of_le_right]
 
   _ = lfpApprox f ⊥ ω := by
-      conv => rhs; unfold lfpApprox
-      aesop (add unsafe (by congr)) (add norm lt_omega0)
+    conv => rhs; unfold lfpApprox
+    aesop (add unsafe congr) (add norm lt_omega0)
 
 theorem fix_eq_order_lfp :
   Lean.Order.fix f f.monotone' = lfp f :=
@@ -110,7 +110,7 @@ theorem fix_eq_order_lfp :
           _ = lfp f     := map_lfp _)
       -- Limit stage
       (hadm := by
-        dsimp [Lean.Order.admissible]
+        unfold Lean.Order.admissible
         intros
         -- Why does aesop refuse to apply sSup_le even when marked as a safe
         -- intro rule?
@@ -124,7 +124,7 @@ theorem fix_eq_order_lfp :
     -- Hence it upper bounds the least fixed point of f
       |> OrderHom.lfp_le_fixed _
 
-  by aesop (add unsafe le_antisymm)
+  le_antisymm ‹_› ‹_›
 
 -- The fixpoint combinator as used in the Lean compiler is denotationally equal
 -- to the transfinite iteration over ordinals up to the Hartog number of the
@@ -138,7 +138,6 @@ lemma fix_mem_range_lfpApprox :
 
 -- Kleene fixpoint theorem, via fixed point iteration over `Ordinal` up to `ω`
 -- as opposed to `ℕ`.
-
 include omega_continuous in
 theorem kleene_fixed_point :
   lfp f = lfpApprox f ⊥ ω :=
@@ -148,9 +147,9 @@ theorem kleene_fixed_point :
         (add unsafe [Monotone.comp, Nat.mono_cast, lfpApprox_monotone]) }
 
   have : f (⨆ n, lfpApprox_Nat n) = ⨆ n, f (lfpApprox_Nat n) := by
-    simp_all [
+    simp_all only [
       ωScottContinuous_iff_map_ωSup_of_orderHom, Chain, ωSup, Chain.map,
-      OrderHom.comp_coe, Function.comp_apply]
+      comp_coe, Function.comp_apply]
 
   lfp_eq_lfpApprox_ord_of_fixed_point <| calc
         f (lfpApprox f ⊥ ω)
@@ -158,18 +157,18 @@ theorem kleene_fixed_point :
     _ = f (⨆ n : ℕ, lfpApprox f ⊥ n) := by rw [lfpApprox_omega0_eq_sSup_lfpApprox_Nat]
 
     _ = ⨆ n : ℕ, f (lfpApprox f ⊥ n) := by
-        simp_all only [OrderHom.coe_mk, Function.comp_apply, lfpApprox_Nat]
+      simp_all only [coe_mk, Function.comp_apply, lfpApprox_Nat]
 
     _ = ⨆ n : ℕ, lfpApprox f ⊥ (n + 1) :=
-        have := lfpApprox_add_one_eq (f := f)
-        by simp_all only [add_one_eq_succ, lfpApprox_Nat]
+      have := lfpApprox_add_one_eq (f := f)
+      by simp_all only [add_one_eq_succ, lfpApprox_Nat]
 
     _ = ⨆ n : ℕ, lfpApprox f ⊥ n :=
-        le_antisymm
-          (sSup_le_sSup λ _ ⟨n, _⟩ ↦ ⟨n + 1, by simp_all only [add_one_eq_succ, Set.mem_range, Nat.cast_add, Nat.cast_one]⟩) <|
-          sSup_le_sSup_of_forall_exists_le λ a ⟨n, h⟩ ↦ by
-            simp_all only [Set.mem_range, add_one_eq_succ, exists_exists_eq_and]
-            exact ⟨n, by rw [←h]; apply lfpApprox_monotone; exact Order.le_succ _⟩
+      le_antisymm
+        (sSup_le_sSup λ _ ⟨n, _⟩ ↦ ⟨n + 1, by simp_all only [add_one_eq_succ, Set.mem_range, Nat.cast_add, Nat.cast_one]⟩) <|
+        sSup_le_sSup_of_forall_exists_le λ a ⟨n, h⟩ ↦ by
+          simp_all only [Set.mem_range, add_one_eq_succ, exists_exists_eq_and]
+          exact ⟨n, by rw [←h]; apply lfpApprox_monotone; exact Order.le_succ _⟩
 
     _ = lfpApprox f ⊥ ω := by rw [lfpApprox_omega0_eq_sSup_lfpApprox_Nat]
 
